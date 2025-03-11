ca77893dd36d672c832746772e6f65dfd1e66b32
sakina <sakinaea-bb104@code.edu.az>
2025-03-12 00:48:48 +0400

    task4 completed

---
+++ task 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+int score = 100;
+    if(score>90 & score<101)
+{
+    Console.WriteLine("baliniz A-dir");
+}
+else if  (score > 80 && score < 91);
+{
+    Console.WriteLine("baliniz b-dir");
+
+ else if (score > 70 && score < 81) ;
+
+    Console.WriteLine("baliniz c-dir");
+
+else if (score > 60 && score < 71) ;
+
+    Console.WriteLine("baliniz d-dir"); 
+    
+
+else if (score > 50 && score < 61) ;
+
+    Console.WriteLine("baliniz e-dir"); }
+
+else ("score<51");
+{
+    Console.WriteLine("KESILDINIZ");
+}
