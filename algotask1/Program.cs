38c208fa9bcb07aaf95b08f9d12f0b54a8cf2b3b
sakina <sakinaea-bb104@code.edu.az>
2025-03-11 22:39:59 +0400

    task1 complete

---
+++ algotask1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Design;
+
+string userName = "Sakina";
+string password = "Sakina123";
+if (password=="Sakina123" && userName=="Sakina123")
+    {
+    Console.WriteLine("Xos gelmissiniz!");
+}
+else
+{
+    Console.WriteLine("userName ve ya password yanlisdir!");
+}
