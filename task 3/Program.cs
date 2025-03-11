362213c1b15557109d235688de9f201e77202f61
sakina <sakinaea-bb104@code.edu.az>
2025-03-12 00:13:33 +0400

    task3 completed

---
+++ task 3/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = 10;
+int num2 = 20;
+if (num1%2==0 && num2%2==0)
+{
+    Console.WriteLine("sum=num1+num2");
+}
+else
+{
+    Console.WriteLine("daxil olunan ededler cut olmalidir!");
+        }
